954abf1aec332e32a715b56d63c3a289c5b84bd2
pratyush770 <pratyushmajumdar27@gmail.com>
2023-08-26 13:34:24 +0530

    first commit

--- templates/feedback.php
+++ templates/feedback.php
@@ -29,7 +29,7 @@ if(isset($_POST['Click'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
      <!--FontAwesome CDN-->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </head>
 <body>
 <div class="mynavbar">
